dee7e30a314127e6c10ef0f61a2990a6dd4233c3
sebv <sebv@sv.home>
2020-01-20 12:28:59 +0100

    update todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // 1. On va analyser les éléments qui vont être modifiés dans la page
+  // On a donc la liste des tâches
+  const [tasks, setTasks] = useState([]);
+  // On a également le champ input
+  const [inputTask, setInputTask] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>To-do list</h1>
+      <ul>
+        {tasks.map((task, index) => {
+          return (
+            <li key={index}>
+              <span
+                className="remove"
+                onClick={() => {
+                  // On crée une copie de tasks
+                  const newTasks = [...tasks];
+                  // On supprime un seul élément à l'index correspondant
+                  newTasks.splice(index, 1);
+                  // 'tasks' est maintenant égal à 'newTasks'
+                  setTasks(newTasks);
+                }}
+              >
+                X
+              </span>
+              <span
+                className={task.isDone === true ? "done" : ""}
+                onClick={() => {
+                  // On créer une copie du tableau
+                  const newTasks = [...tasks];
+                  // task.isDone est égal à différent de task.isDone
+                  task.isDone = !task.isDone;
+                  setTasks(newTasks);
+                }}
+              >
+                {task.name}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+      <form
+        onSubmit={event => {
+          // Empêche le rechargement de la page
+          event.preventDefault();
+          // On va créer une copie de 'tasks' car la liste va être mise à jour
+          const newTasks = [...tasks];
+          // On va créer un objet qui contiendra une clé 'name' + ce que contiendra le champ 'inputTasks'
+          newTasks.push({ name: inputTask, isDone: false });
+          // On dit à React que 'newTasks' est maintenant égal à 'tasks'
+          setTasks(newTasks);
+          // On réinitialise la champ input à vide
+          setInputTask("");
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Ajouter une tâche"
+          value={inputTask}
+          onChange={event => {
+            setInputTask(event.target.value);
+          }}
+        />
+        <br />
+        <input type="submit" value="Soumettre la tâche" />
+      </form>
     </div>
   );
 }
